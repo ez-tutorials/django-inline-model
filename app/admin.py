1893597baa150ac48e8bc15239dd45b36d3d7bb4
En Zhou <en.zhou@spandigital.com>
2019-11-06 20:05:05 +0200

    Added user and data fixtures; Added customized messsage in OrderedItemAdmin; Added override function save_model();

--- app/admin.py
+++ app/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
+from django.contrib import messages
 
 # Register your models here.
-from .models import Group, Membership, Person
-from .models import Role, Movie, Artist
-from .models import Book, Author
-from .models import Part, Product, Warehouse, Component
+# from .models import Group, Membership, Person
+# from .models import Role, Movie, Artist
+# from .models import Book, Author
+from .models import Part, Product, Warehouse, Component, Batch, Supplier
+from .models import Person, Status, Client, Order, OrderedItem
 
 from django.contrib import admin
-from django_admin_listfilter_dropdown.filters import DropdownFilter, RelatedDropdownFilter, ChoiceDropdownFilter
-
-
 
 
 class CustomModelAdmin(object):
@@ -20,49 +19,30 @@ class CustomModelAdmin(object):
         super(CustomModelAdmin, self).__init__(model, admin_site)
 
 
-@admin.register(Group)
-class GroupAdmin(CustomModelAdmin, admin.ModelAdmin):
+class BatchAdmin(admin.ModelAdmin):
     pass
+admin.site.register(Batch, BatchAdmin)
 
 
-@admin.register(Person)
-class PersonAdmin(CustomModelAdmin, admin.ModelAdmin):
+class SupplierAdmin(admin.ModelAdmin):
     pass
+admin.site.register(Supplier, SupplierAdmin)
 
 
-@admin.register(Membership)
-class MembershipAdmin(CustomModelAdmin, admin.ModelAdmin):
+class ClientAdmin(admin.ModelAdmin):
     pass
+admin.site.register(Client, ClientAdmin)
 
 
-class RoleInline(admin.TabularInline):
-    model = Role
-    extra = 1
+class StatusAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Status, StatusAdmin)
 
 
-class ArtistAdmin(admin.ModelAdmin):
-    inlines = (RoleInline,)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Person, PersonAdmin)
 
-
-class MovieAdmin(admin.ModelAdmin):
-    inlines = (RoleInline,)
-
-
-admin.site.register(Movie, MovieAdmin)
-admin.site.register(Artist, ArtistAdmin)
-admin.site.register(Role)
-
-
-class BookInline(admin.TabularInline):
-    model = Book
-    extra = 1
-
-
-class AuthorAdmin(admin.ModelAdmin):
-    inlines = [
-        BookInline,
-    ]
-admin.site.register(Author, AuthorAdmin)
 
 
 class PartAdmin(admin.ModelAdmin):
@@ -74,17 +54,12 @@ class ComponentInline(admin.TabularInline):
     model = Component
     extra = 1
 
-    # list_field = ['part', 'warehouse', 'quantity']
-
-    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
-    #     if db_field.name == "warehouse":
-    #         kwargs["queryset"] = Part.objects.filter(warehouse=db_field.value)
-    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 class ProductAdmin(admin.ModelAdmin):
     inlines = [
         ComponentInline,
     ]
+
 
 admin.site.register(Product, ProductAdmin)
 
@@ -92,10 +67,49 @@ admin.site.register(Product, ProductAdmin)
 class WarehouseAdmin(admin.ModelAdmin):
     pass
 
+
 admin.site.register(Warehouse, WarehouseAdmin)
 
 
-class ComponentAdmin(admin.ModelAdmin):
-    pass
+# class ComponentAdmin(admin.ModelAdmin):
+#     pass
+#
+# admin.site.register(Component, ComponentAdmin)
 
-admin.site.register(Component, ComponentAdmin)
+
+
+class OrderedItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'quantity']
+
+
+admin.site.register(OrderedItem, OrderedItemAdmin)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderedItem
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline,
+    ]
+
+    def save_formset(self, request, form, formset, change):
+        # formset has to be saved to get correct queryset
+        formset.save()
+        # Update part status and quantity available here
+        ordered_items = [f if f.quantity != 0 else f for f in formset.queryset]
+        for item in ordered_items:
+            quantity = item.quantity
+            product = item.product
+            # Update part total available quantity if all part check passes
+        # messages.add_message(request, messages.ERROR, 'Car has been sold')
+        super().save_formset(request, form, formset, change)
+
+    def save_model(self, request, obj, form, change):
+        # Update order status
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Order, OrderAdmin)
--- app/models.py
+++ app/models.py
@@ -4,93 +4,167 @@ from smart_selects.db_fields import GroupedForeignKey, ChainedForeignKey
 # Create your models here.
 
 
+#
+#
+# class Group(models.Model):
+#     name = models.CharField(max_length=128)
+#     members = models.ManyToManyField(
+#         Person,
+#         through='Membership',
+#         through_fields=('group', 'person'),
+#     )
+#
+#
+# class Membership(models.Model):
+#     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+#     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+#     inviter = models.ForeignKey(
+#         Person,
+#         on_delete=models.CASCADE,
+#         related_name="membership_invites",
+#     )
+#     invite_reason = models.CharField(max_length=64)
+#
+#
+# class Artist(models.Model):
+#     name = models.CharField(max_length=200)
+#
+#
+# class Movie(models.Model):
+#     title = models.CharField(max_length=100)
+#     artists = models.ManyToManyField(Artist, related_name='actor')
+#
+#
+# class Role(models.Model):
+#     role_name = models.CharField(max_length=100)
+#     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+#     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+#
+#
+#
+#
+#
+# class Author(models.Model):
+#    name = models.CharField(max_length=100)
+#
+# class Book(models.Model):
+#    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+#    title = models.CharField(max_length=100)
+
+
 class Person(models.Model):
-    name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    mobile = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
-class Group(models.Model):
-    name = models.CharField(max_length=128)
-    members = models.ManyToManyField(
-        Person,
-        through='Membership',
-        through_fields=('group', 'person'),
-    )
+class Status(models.Model):
+    # Available, Delivered, Ordered
+    name = models.CharField(max_length=255, default='Available')
 
-
-class Membership(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    inviter = models.ForeignKey(
-        Person,
-        on_delete=models.CASCADE,
-        related_name="membership_invites",
-    )
-    invite_reason = models.CharField(max_length=64)
-
-
-class Artist(models.Model):
-    name = models.CharField(max_length=200)
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    artists = models.ManyToManyField(Artist, related_name='actor')
-
-
-class Role(models.Model):
-    role_name = models.CharField(max_length=100)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-
-
-
-
-
-class Author(models.Model):
-   name = models.CharField(max_length=100)
-
-class Book(models.Model):
-   author = models.ForeignKey(Author, on_delete=models.CASCADE)
-   title = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Warehouse(models.Model):
-   name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True, null=True)
 
-   def __str__(self):
-       return f"{self.name}"
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    contact_person = models.ForeignKey(Person, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    contact_person = models.ForeignKey(
+        Person, on_delete=models.DO_NOTHING, blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Batch(models.Model):
+    number = models.CharField(max_length=255)
+    manufacture_date = models.DateField()
+    expire_date = models.DateField()
+    supplier = models.ForeignKey(Supplier, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.number} from {self.supplier.name}"
 
 
 class Product(models.Model):
-   name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
-   def __str__(self):
-       return f"{self.name}"
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Part(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
+    available_total = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.name} in {self.warehouse}"
+        return f"{self.name} - {self.batch.number} stored in " \
+               f"{self.warehouse}, {self.available_total} available."
 
 
 class Component(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE, related_name='warehouse')
-    # grouped_part = GroupedForeignKey(Part, 'warehouse')
-    part = ChainedForeignKey(
-        Part,
-        chained_field="warehouse",
-        chained_model_field="warehouse",
-        show_all=True,
-        auto_choose=True,
-        sort=True
-    )
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    # warehouse = models.ForeignKey(Warehouse, on_delete=models.DO_NOTHING, related_name='warehouse')
+    part = GroupedForeignKey(Part, 'warehouse')
+    # batch = GroupedForeignKey(Batch, 'supplier')
     quantity = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.product} in {self.warehouse}"
+        part_names = []
+        if self.part:
+            p_name = Part.objects.get(id=self.part.id).name
+            part_names.append(p_name)
+        return f"{self.product.name}: {part_names}"
 
+
+# Order model on save()
+#   Based on Product and its part, update the
+
+class Order(models.Model):
+    order_name = models.CharField(max_length=255, default="Order")
+    delivered_by = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        product_list = []
+        # if self.product:
+        return f"{self.order_name}"
+
+
+class OrderedItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+
+    # product = GroupedForeignKey(Product, "status")
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        product_names = []
+        if self.product:
+            p_name = Product.objects.get(id=self.product.id).name
+            product_names.append(p_name)
+        return f"{product_names} - {self.quantity}"
 
